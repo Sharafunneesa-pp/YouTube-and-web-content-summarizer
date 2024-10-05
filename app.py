b5f8991630844777c4156f82fdfc4de14514d6bd
Sharafunneesa-pp <127863253+Sharafunneesa-pp@users.noreply.github.com>
2024-10-05 23:58:50 +0400

    code updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLo
 
 
 ## sstreamlit APP
-st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.set_page_config(page_title="LangChain: Summarize Text From Youtube or Website", page_icon="🦜")
 st.title("🦜 LangChain: Summarize Text From YT or Website")
 st.subheader('Summarize URL')
 
